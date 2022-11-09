e1c86335f097d5752908da226c0f8df3069df4b2
alexbarros <114031281+alexinventor@users.noreply.github.com>
2022-11-09 12:16:47 -0300

    Create Desenvolvimento9.py
    Código de contagem regressiva

---
+++ Desenvolvimento9.py
@@ -0,0 +1,16 @@
+# Desenvolvimento 9
+# autor: Alex Barros
+# Data: 09/11/2022
+
+import time
+
+inicioContagem = 10
+fimContagem = 0
+
+print("iniciando contagem regressiva!")
+
+for i in range(inicioContagem,fimContagem-1,-1):
+    print(i)
+    time.sleep(1)
+
+print("BUM!!!")
